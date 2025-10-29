d841823e2b11e2e741ae539333a2940fa2d23840
rapha4lx <rapha4.lx@gmail.com>
2025-10-29 14:18:07 -0300

    [+] Created line_add_back; [+] Created line clear;

--- src/parser/map/line.c
+++ src/parser/map/line.c
@@ -2,7 +2,17 @@
 
 void t_line_add_back(t_line **lst, t_line *new)
 {
+    t_line *tmp;
 
+    if (!lst)
+    {
+        lst = &new;
+        return ;
+    }
+    tmp = *lst;
+    while (tmp->next)
+        tmp = tmp->next;
+    tmp->next = new;
 }
 
 t_line *t_line_new(char *line)
@@ -17,3 +27,19 @@ t_line *t_line_new(char *line)
     return (new);
 }
 
+void t_line_clear(t_line **lst)
+{
+    t_line *tmp;
+
+    if (!lst)
+        return ;
+    tmp = *lst;
+    while (tmp)
+    {
+        *lst = tmp->next;
+        free(tmp->line);
+        free(tmp);
+        tmp = *lst;
+    }
+    *lst = NULL;
+}

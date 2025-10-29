77eff9930426580ea812ca3d3842c66b7549461b
rapha4lx <rapha4.lx@gmail.com>
2025-10-29 14:18:31 -0300

    feat: Created alloc_texture and free_texture

---
+++ src/parser/map/texture.c
@@ -0,0 +1,38 @@
+#include "cube.h"
+
+int alloc_texture(t_texture *texture, char *line)
+{
+    if (!line || !line[0] || !line[1])
+        return;
+    if (line[0] == 'N' && line[1] == 'O')
+        texture->north = ft_strdup(line + 2);
+    else if (line[0] == 'S' && line[1] == 'O')
+        texture->south = ft_strdup(line + 2);
+    else if (line[0] == 'W' && line[1] == 'E')
+        texture->weast = ft_strdup(line + 2);
+    else if (line[0] == 'E' && line[1] == 'A')
+        texture->east = ft_strdup(line + 2);
+    else if (line[0] == 'F' && line[1] == ' ')
+        texture->floor = ft_strdup(line + 2);
+    else if (line[0] == 'C' && line[1] == ' ')
+        texture->ceiling = ft_strdup(line + 2);
+    else 
+        return 0;
+    return 1;
+}
+
+void free_texture(t_texture *texture)
+{
+    if (texture->north)
+        free(texture->north);
+    if (texture->south)
+        free(texture->south);
+    if (texture->weast)
+        free(texture->weast);
+    if (texture->east)
+        free(texture->east);
+    if (texture->floor)
+        free(texture->floor);
+    if (texture->ceiling)
+        free(texture->ceiling);
+}
